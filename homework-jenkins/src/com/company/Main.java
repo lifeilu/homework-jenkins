9418714aa885c4c8d90d294e5910beb9f4f100cc
路丽菲 <lulifei@lulifeideMacBook-Pro.local>
2017-07-24 13:54:19 +0800

    change

--- homework-jenkins/src/com/company/Main.java
+++ homework-jenkins/src/com/company/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	// write your code here
         System.out.println("Hello World!");
         Max myTest = new Max();
-        int maxNum = myTest.getMax(1, 2);
+        int maxNum = myTest.getMax(3, 2);
         System.out.println(maxNum);
     }
 }
